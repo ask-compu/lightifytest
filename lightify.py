6a3a61491003d5cbf9f267810553b772d2747a31
ask-compu <hotwheels14901@me.com>
2016-01-17 20:21:46 -0500

    some decoding

--- lightify.py
+++ lightify.py
@@ -276,7 +276,7 @@ class Lightify:
             payload = data[pos:pos+18]
 
             (idx, name) = struct.unpack("<H16s", payload)
-            name = str(name).replace('\0', "")
+            name = name.decode("utf-8", "ignore").replace('\0', "")
 
             groups[idx] = name
             self.__logger.debug("Idx %d: '%s'", idx, name)
@@ -303,7 +303,7 @@ class Lightify:
         data = self.recv()
         payload = data[7:]
         (idx, name, num) = struct.unpack("<H16sB", payload[:19])
-        name = str(name).replace('\0', "")
+        name = name.decode("utf-8", "ignore").replace('\0', "")
         self.__logger.debug("Idx %d: '%s' %d", idx, name, num)
         for i in range(0,num):
             pos = 7 + 19 + i * 8
@@ -376,7 +376,7 @@ class Lightify:
             self.__logger.debug("%d %d %d", i, pos, len(payload))
 
             (a,addr,status,name,extra) = struct.unpack("<HQ16s16sQ", payload)
-            name = str(name).replace('\0', "")
+            name = name.decode("utf-8", "ignore").replace('\0', "")
 
             self.__logger.debug('light: %x %x %s %x', a, addr, name, extra)
             if addr in old_lights:
